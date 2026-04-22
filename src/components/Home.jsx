9df83283338feeb046a428c473e0baa2bece5f93
Vanessa Heredia <vanessaheredia444@gmail.com>
2026-04-22 17:45:15 -0500

    Sum two numbers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ export default function Home({setPantalla}){
         <div>
         <h1>Home</h1>
         <button onClick={() => setPantalla("Sum")}>Go to Sum</button>
+        <p></p>
         <button onClick={() => setPantalla("Calculator")}>Go to Calculator</button>
         </div>
     );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,9 +1,20 @@
+import {useState} from "react" 
 export default function Sum({setPantalla}){
     const [num1,setNum1]=useState("");
     const [num2,setNum2]=useState("");
+    const sumar=()=>{
+        const resultado=parseFloat(num1)+parseFloat(num2);
+        alert("The result is: "+resultado);
+    };
     return(
         <div>
         <h1>Sum</h1>
+        <input type="number" value={num1} onChange={(e)=>setNum1(e.target.value)} placeholder="Number 1"/>
+        <p></p>
+        <input type="number" value={num2} onChange={(e)=>setNum2(e.target.value)} placeholder="Number 2"/>
+        <p></p>
+        <button onClick={sumar}>Sum</button>
+        <p></p>                    
         <button onClick={() => setPantalla("Home")}>Go to Home</button>
         </div>
     );
